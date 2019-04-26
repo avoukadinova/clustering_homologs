688073cf0b2f8cc2ab022070c17719050aae8026
briannho <47989819+briannho@users.noreply.github.com>
2019-04-26 11:00:30 -0500

    Update recluster.py

--- recluster.py
+++ recluster.py
@@ -11,10 +11,10 @@ with open('homo_out.txt') as handle:
    output = list(reader)
 
 for line in output:
-
-   cluster1 = './USEARCH_results/' + line[0]
-   cluster2 = './USEARCH_results/' + line[1]
-   cmd = 'cat ' + cluster2 + '>> ' + cluster1
-   os.system(cmd)
-   os.system('rm ' + cluster2)
+   cluster1 = './USEARCH_results/' + line[0] 
+   for name in line[1:]:
+      cluster2 = './USEARCH_results/' + name
+      cmd = 'cat ' + cluster2 + '>> ' + cluster1
+      os.system(cmd)
+      os.system('rm ' + cluster2)
 
